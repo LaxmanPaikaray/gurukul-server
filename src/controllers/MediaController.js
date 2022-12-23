1c3baa6a4271b75cd97e6e2c3704781eb79ae843
LaxmanPaikaray <paikaraylaxman@gmail.com>
2022-12-22 19:15:31 -0500

    file upload restored

--- src/controllers/MediaController.js
+++ src/controllers/MediaController.js
@@ -18,46 +18,46 @@ const utils = require( '../../system/helpers/Utility' ),
 class MediaController extends Controller {
 
     // file upload using multer
-    // storage = multer.diskStorage( {
-    //     'destination': function( req, file, cb ) {
-    //         const dir = config.UPLOAD_PATH;
+    storage = multer.diskStorage( {
+        'destination': function( req, file, cb ) {
+            const dir = config.UPLOAD_PATH;
 
-    //         fs.exists( dir, ( exist ) => {
-    //             if ( !exist ) {
-    //                 return fs.mkdir( dir, ( error ) => cb( error, dir ) );
-    //             }
-    //             return cb( null, dir );
-    //         } );
-    //     },
-    //     'filename': function( req, file, cb ) {
-    //         const fileOriginalName = utils.slugify( file.originalname );
+            fs.exists( dir, ( exist ) => {
+                if ( !exist ) {
+                    return fs.mkdir( dir, ( error ) => cb( error, dir ) );
+                }
+                return cb( null, dir );
+            } );
+        },
+        'filename': function( req, file, cb ) {
+            const fileOriginalName = utils.slugify( file.originalname );
 
-    //         cb( null, `${( new Date() ).getTime() }-${ fileOriginalName}` );
-    //     }
-    // } );
-    // sftp settings     
-    upload = multer({
-        storage: new FTPStorage({
-          destination: function (req, file, options, callback) {
-                callback(null, "gurukuledu.in/uploads/"+path.join(options.basepath, Date.now()+file.originalname))
-            },
-          ftp: {
-            secure: false, // enables FTPS/FTP with TLS
-            host:'nilachal.org',
-            user:'nilachal',
-            password:"Fyrm052iL3"
-          }
-        }),
-        'limits': {
-            'fileSize': 1024 * 1024 * 5
+            cb( null, `${( new Date() ).getTime() }-${ fileOriginalName}` );
         }
-      })
-    // upload = multer( {
-    //     'storage': this.storage,
+    } );
+    // sftp settings     
+    // upload = multer({
+    //     storage: new FTPStorage({
+    //       destination: function (req, file, options, callback) {
+    //             callback(null, "gurukuledu.in/uploads/"+path.join(options.basepath, Date.now()+file.originalname))
+    //         },
+    //       ftp: {
+    //         secure: false, // enables FTPS/FTP with TLS
+    //         host:'nilachal.org',
+    //         user:'nilachal',
+    //         password:"Fyrm052iL3"
+    //       }
+    //     }),
     //     'limits': {
     //         'fileSize': 1024 * 1024 * 5
     //     }
-    // } );
+    //   })
+    upload = multer( {
+        'storage': this.storage,
+        'limits': {
+            'fileSize': 1024 * 1024 * 5
+        }
+    } );
 
     constructor( service ) {
         super( service );
@@ -67,10 +67,10 @@ class MediaController extends Controller {
     async insert( req, res, next ) {
         try {
             const uploadPath = config.UPLOAD_PATH;
-            // req.file.path = req.file.path.split( `${uploadPath }/` )[ 1 ];
+            req.file.path = req.file.path.split( `${uploadPath }/` )[ 1 ];
             const response = await this.service.insert( req.file );
-            // const upload = await this.service.transferFileToDomain(`${uploadPath }/${req.file.filename}`, `${req.file.filename}`)
-
+            const upload = await this.service.transferFileToDomain(`${uploadPath }/${req.file.filename}`, `${req.file.filename}`)
+            
             return res.status( response.statusCode ).json( response );
         } catch ( e ) {
             next( e );
